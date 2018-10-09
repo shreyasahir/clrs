c81366e34b6ada2d1a12f90ddb660fd8540d764c
श्रेयस <shreyasahir@users.noreply.github.com>
2018-10-09 08:05:00 -0700

    Update insertion.go

--- sorting/insertion.go
+++ sorting/insertion.go
@@ -4,23 +4,24 @@ import (
 	"fmt"
 )
 
-func insert(x []int) []int {
-	i := 1
-	for i < len(x) {
-		j := i
-		for j > 0 && x[j-1] > x[j] {
-			x[j], x[j-1] = x[j-1], x[j]
-			j--
-			fmt.Println(x)
-		}
-		i++
-	}
-	return x
-}
-
 func main() {
 	var x = []int{5, 2, 4, 6, 1, 3}
 	fmt.Println(x)
-	insert(x)
-	fmt.Println(x)
+	fmt.Println(insertSort(x))
+}
+
+func insertSort(arr []int) []int {
+	i := 1
+	for i < len(arr) {
+		key := arr[i]
+
+		j := i - 1
+		for j >= 0 && arr[j] > key {
+			arr[j+1] = arr[j]
+			j--
+		}
+		arr[j+1] = key
+		i++
+	}
+	return arr
 }
